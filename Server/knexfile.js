ffd3864cac09788615ef05efe2b3a7617f1e69c3
Richard <huffmanra12@gmail.com>
2023-07-20 23:01:16 -0600

    modified docker-compose file

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: 'docker-db',
+      host: '127.0.0.1',
       port: 5432,
       user: 'postgres',
       password: 'docker',
